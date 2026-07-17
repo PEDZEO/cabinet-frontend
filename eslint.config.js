d286e49ed2dc53d57adfd56c2ccad0885b816c02
PEDZEO <vitali.gavrikov@yandex.ru>
2026-07-17 03:58:52 +0300

    chore: update audited frontend dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ['dist', 'node_modules', 'public'],
+    ignores: ['dist', 'node_modules', 'public', 'playwright-report', 'test-results'],
   },
   {
     files: ['**/*.{ts,tsx}'],
